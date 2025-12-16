0e09396b97566f258e342481910fa1fb4c41c6ad
Akrom Khasani <akrom.khasani@shortlyst.ai>
2025-12-16 10:28:25 +0700

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { ConsumerConfirm, MessageBus } from './generic'
 import { RabbitMqMessageBus } from './rabbitmq'
+import { SupabaseMessageBus } from './supabase'
 
-export { ConsumerConfirm, MessageBus, RabbitMqMessageBus }
+export { ConsumerConfirm, MessageBus, RabbitMqMessageBus, SupabaseMessageBus }
